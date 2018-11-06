77190500bbfcde7ce8cd5e6d489df96df0488c4f
Victor Marta <vicm159@gmail.com>
2018-11-05 19:41:01 -0800

    hw

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,4 @@
+// my solution
 // function bubbleSort(array) {
 //   if (array.length === 0) {
 //     return array;
@@ -20,42 +21,69 @@
 //   }
 // }
 
+// solution from hw
 function bubbleSort(array) {
-  if (array.length === 0) {
-    return array;
-  } else {
-    let swap = function(array, position1, position2) {
-      let temp = array[position1];
-      array[position1] = array[position2];
-      array[position2] = temp;
-    };
-    let compar = function(arr1, arr2) {
-      for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-          return false;
-        }
-      }
-      return true;
-    };
-
-    let end_index = array.length;
-    let old_array = [];
-    while (
-      compar(old_array, array) //for (let i = 0; i < array.length; i++)
-    ) {
-      console.log(old_array);
-      for (let j = 0; j < end_index; j++) {
-        if (array[j] > array[j + 1]) {
-          swap(array, j, j + 1);
-        }
-      }
-      // if (compar(old_array, array)) {
-      //   break;
-      // }
-      old_array = array.slice();
-      end_index--;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; i < array.length; j++) {
+      if (!inOrder(array, j)) swap(array, j);
     }
-    return array;
   }
+
+  return array;
 }
-bubbleSort([1, 3, 5, 4, 2]);
+
+function inOrder(array, index) {
+  // pure function
+  if (index === array.length - 1) return true;
+  return array[index] < array[index + 1];
+}
+
+function swap(array, index) {
+  // inpure function
+  let oldLeftValue = array[index];
+  array[index] = array[index + 1];
+  array[index + 1] = oldLeftValue;
+}
+
+// function bubbleSort(array) {
+//   if (array.length === 0) {
+//     return array;
+//   } else {
+//     let swap = function(array, position1, position2) {
+//       let temp = array[position1];
+//       array[position1] = array[position2];
+//       array[position2] = temp;
+//     };
+//     let compar = function(arr1, arr2) {
+//       for (let i = 0; i < arr1.length; i++) {
+//         if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+//           return false;
+//         }
+//       }
+//       return true;
+//     };
+
+//     let end_index = array.length;
+//     let start = true;
+//     let old_array = array.slice();
+//     while (
+//       !compar(old_array, array) ||
+//       start //for (let i = 0; i < array.length; i++)
+//     ) {
+//       start = false;
+//       let old_array = array.slice();
+//       for (let j = 0; j < end_index; j++) {
+//         if (array[j] > array[j + 1]) {
+//           swap(array, j, j + 1);
+//         }
+//       }
+//       // if (compar(old_array, array)) {
+//       //   break;
+//       // }
+
+//       end_index--;
+//     }
+//     return array;
+//   }
+// }
+// console.log(bubbleSort([1, 3, 5, 4, 2]));
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,7 +2,12 @@ describe('Bubble Sort', function() {
   it('handles an empty array', function() {
     expect(bubbleSort([])).toEqual([]);
   });
+
+  it('sorts an array of one element', function() {
+    expect(bubbleSort([8])).toEqual([8]);
+  });
+
   it('handles large arrary', function() {
-    expect(bubbleSort([3, 5, 2, 1])).toEqual([1, 2, 3, 5]);
+    expect(bubbleSort([3, 5, 2, 9, 1])).toEqual([1, 2, 3, 5, 9]);
   });
 });
